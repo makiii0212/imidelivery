a43ee548d0615de6a65dd96e9ccf7b2122d10cc9
Marta Tripkovic <martatripkovic@gmail.com>
2025-07-15 11:39:13 +0200

    Added sign up button

--- delivery-frontend/src/components/Header.jsx
+++ delivery-frontend/src/components/Header.jsx
@@ -7,9 +7,10 @@ function Header() {
     <header className="app-header">
       <img src={logo} alt="Logo" className="logo" />
         <nav className="header-nav">
-        <a href="/">Pocetna</a>
-        <a href="/faq">Cesta pitanja</a>
-        <a href="/about">O nama</a>
+          <a href="/">Pocetna</a>
+          <a href="/faq">Cesta pitanja</a>
+          <a href="/about">O nama</a>
+          <button className="signup-button"><a href="/signup">Sign up</a></button>
       </nav>
     </header>
   );
