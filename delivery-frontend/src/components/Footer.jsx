a3bf901178fa15780b9f0766263f50e07af95595
Marta Tripkovic <martatripkovic@gmail.com>
2025-07-16 15:47:11 +0200

    Added Footer.jsx and Footer.css

---
+++ delivery-frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="app-footer">
+          <span>© 2025 | Institut za matematiku i informatiku</span>
+    </footer>
+  );
+}
+
+export default Footer;
